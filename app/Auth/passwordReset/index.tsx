670e05d75b1637c5cdd1a33e5864e51d45261a10
stepheeeen <stephenon56@gmail.com>
2024-09-14 16:04:54 +0100

    Password Reset page - complete

--- app/Auth/passwordReset/index.tsx
+++ app/Auth/passwordReset/index.tsx
@@ -2,20 +2,29 @@ import { DefaultButton } from "@/component/reusable/Button";
 import { DefaultInput, PasswordInput } from "@/component/reusable/Input";
 import { useRouter } from "expo-router";
 import { Pressable, Text, TextInput, View } from "react-native";
+import { BsArrowLeft } from "react-icons/bs";
 import tailwind from "twrnc";
 
 const index = () => {
   const router = useRouter();
   return (
     <View style={tailwind`h-full bg-[#0F1219] w-full px-1 py-5`}>
-      <View>
-        <Text style={tailwind`text-white font-bold text-[27px]`}>
-          Reset Your Password
+      <View
+        style={tailwind`flex flex-row items-center w-full justify-between px-3 py-6 h-[13%] absolute top-0 bg-[#0F1219] z-10 `}
+      >
+        <Pressable onPress={()=>{router.back}}>
+          <BsArrowLeft size={30} color="white" />
+        </Pressable>
+
+        <Text style={tailwind`ml-[-25px] font-semibold text-[22px] text-white`}>
+          Reset
         </Text>
+
+        <Text></Text>
       </View>
 
-      <View style={tailwind`w-full mt-[10%] mb-6 px-3`}>
-        <Text style={tailwind`text-white font-normal text-[18px] mt-2 mb-4`}>
+      <View style={tailwind`w-full mt-[20%] mb-2 px-3`}>
+        <Text style={tailwind`text-white font-normal text-[16px] mt-2 mb-4`}>
           Enter your email address and we’ll send you a password reset link
         </Text>
       </View>
@@ -26,7 +35,7 @@ const index = () => {
         customCss="w-[95%] mx-auto"
       />
 
-      <View style={tailwind`mt-10 w-[95%] mx-auto`}>
+      <View style={tailwind`mt-8 w-[95%] mx-auto`}>
         <DefaultButton
           onPress={() => {
             router.push("/Auth/OTP");
