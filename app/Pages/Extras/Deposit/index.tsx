009fc85b5e1f000a8e395df8386c5b10ee9c3077
stepheeeen <stephenon56@gmail.com>
2024-11-06 22:58:41 +0100

    payment and verification through paystack

--- app/Pages/Extras/Deposit/index.tsx
+++ app/Pages/Extras/Deposit/index.tsx
@@ -1,17 +1,24 @@
 import { DefaultButton } from "@/component/reusable/Button";
 import { DefaultInput } from "@/component/reusable/Input";
 import { useRouter } from "expo-router";
-import { Alert } from "react-native";
-import { useState } from "react";
+import {
+  Alert,
+  ActivityIndicator,
+  Modal,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
+import { useEffect, useState } from "react";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { Pressable, Text, View, ActivityIndicator, Modal } from "react-native";
-import tailwind from "twrnc";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import { DisabledButton } from "@/component/reusable/DisabledButton";
 import axios from "axios";
 import { baseUrl } from "@/app/constants";
 import { WebView } from "react-native-webview";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import tailwind from "twrnc";
+import { Toast } from "toastify-react-native";
 
 const Index = () => {
   const [coin, setCoin] = useState("");
@@ -21,56 +28,73 @@ const Index = () => {
   const [showPaystack, setShowPaystack] = useState(false);
   const [paymentUrl, setPaymentUrl] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const [reference, setReference] = useState("");
   const router = useRouter();
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Fetch token once on component mount
+    const fetchToken = async () => {
+      const storedToken = await AsyncStorage.getItem("accessToken");
+      setToken(storedToken);
+    };
+    fetchToken();
+  }, []);
+
+  const verifyPayment = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `${baseUrl}/wallet/verify/${reference}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      Alert.alert("Payment successfull", response.data.message);
+      // if (response.data.status === "200") {
+      // } else {
+      //   Alert.alert("Payment verification error:", response.data.message);
+      // }
+    } catch (error: any) {
+      Alert.alert("Payment verification error:", error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const calculateAmount = (coinValue: string) =>
+    (Number(coinValue) * 25).toString();
+  const calculateCoin = (amountValue: string) =>
+    (Number(amountValue) / 25).toString();
 
   const handleCoinChange = (value: string) => {
     setCoin(value);
-    const nairaAmount = Number(value) * 25;
-    setAmount(nairaAmount.toString());
+    setAmount(calculateAmount(value));
     setIsCoinValid(Number(value) >= 1);
   };
 
   const handleAmountChange = (value: string) => {
     setAmount(value);
-    const coinValue = Number(value) / 25;
-    setCoin(coinValue.toString());
+    setCoin(calculateCoin(value));
     setIsAmountValid(Number(value) >= 25);
   };
 
   const handlePaystackPayment = async () => {
-    if (isAmountValid && isCoinValid) {
-      setLoading(true); // Show loading indicator
-      
+    if (isAmountValid && isCoinValid && token) {
       try {
-        const token = await AsyncStorage.getItem("accessToken");
-        // console.log(token)
-
-        // Send request to get the Paystack authorization URL
-        const response = await axios.post(`${baseUrl}/wallet/fund`, {
-          amount,
-          headers: {
-            authorization: `Bearer ${token}`,
-          },
-        });
-        response.data;
-        console.log(response.data);
-
-        // Open the Paystack payment URL inside WebView
-        // const authorizationUrl = response.data.authorization_url;
-        // setPaymentUrl(authorizationUrl); // Set the payment URL for the WebView
-        // setShowPaystack(true); // Show the WebView
-      } catch (error: any) {
-        console.error(error.response.data);
-        // console.error(
-        //   "Payment initiation error:",
-        //   error.response?.data?.message || error.message
-        // );
-        // Alert.alert(
-        //   "Error",
-        //   "An error occurred while processing your payment."
-        // );
+        setLoading(true);
+        const response = await axios.post(
+          `${baseUrl}/wallet/fund`,
+          { amount },
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        setPaymentUrl(response.data.authorization_url);
+        setReference(response.data.reference);
+        setShowPaystack(true);
+      } catch (error) {
+        console.error("Payment initiation error:", error);
       } finally {
-        setLoading(false); // Hide loading indicator
+        setLoading(false);
       }
     }
   };
@@ -78,96 +102,86 @@ const Index = () => {
   const handleWebViewClose = () => {
     setShowPaystack(false);
     setPaymentUrl(null);
+    verifyPayment();
   };
 
   return (
-    <>
-      <View style={tailwind`h-full bg-[#0F1219] w-full px-1 py-5`}>
-        <View style={tailwind`w-full mt-[5%] mb-[4%] px-3`}>
-          <Text style={tailwind`text-white font-bold text-[27px] text-center`}>
-            Deposit
-          </Text>
-        </View>
-
-        <DefaultInput
-          customInput={!isCoinValid ? "border-red-500" : ""}
-          label="Coin"
-          placeholder="1"
-          customCss="w-[95%] mx-auto"
-          value={coin}
-          onChangeText={handleCoinChange}
-        />
-
-        <View
-          style={tailwind`w-full flex flex-row justify-center items-center mt-5`}
-        >
-          <View
-            style={tailwind`w-auto flex flex-row justify-center items-center gap-2`}
-          >
-            <FontAwesome6 name="arrow-down" size={24} color="white" />
-            <FontAwesome6 name="arrow-up" size={24} color="white" />
-          </View>
-        </View>
-
-        <DefaultInput
-          customInput={!isAmountValid ? "border-red-500" : ""}
-          label="Amount (Naira)"
-          placeholder="25"
-          customCss={`w-[95%] mx-auto mt-5`}
-          value={amount}
-          onChangeText={handleAmountChange}
-        />
-
-        <View style={tailwind`mt-10 w-[95%] mx-auto`}>
-          {!(isAmountValid && isCoinValid) ? (
-            <DisabledButton text="Continue" />
-          ) : (
-            <DefaultButton onPress={handlePaystackPayment} text="Continue" />
-          )}
-
-          <Pressable onPress={() => router.push("/Pages/")}>
-            <Text
-              style={tailwind`text-[#00A859] ml-2 mx-auto mt-3 text-[18px] underline`}
-            >
-              Cancel
-            </Text>
-          </Pressable>
-        </View>
-
-        <Text>
-          {loading && <ActivityIndicator size="large" color="green" />}{" "}
+    <View style={tailwind`h-full bg-[#0F1219] w-full px-1 py-5`}>
+      <View style={tailwind`w-full mt-[5%] mb-[4%] px-3`}>
+        <Text style={tailwind`text-white font-bold text-[27px] text-center`}>
+          Deposit
         </Text>
-        {/* Show loading indicator */}
-
-        {showPaystack && paymentUrl && (
-          <Modal
-            visible={showPaystack}
-            animationType="slide"
-            style={tailwind`pt-7`}
-          >
-            <WebView
-              style={tailwind`mt-[18%]`}
-              source={{ uri: paymentUrl }}
-              onNavigationStateChange={(navState: any) => {
-                if (navState.url.includes("payment successful")) {
-                  handleWebViewClose();
-                  Alert.alert(
-                    "Payment Verified",
-                    "Your payment has been successfully verified!"
-                  );
-                }
-              }}
-            />
-            <Pressable
-              style={tailwind`absolute top-10 right-10`}
-              onPress={handleWebViewClose}
-            >
-              <MaterialIcons name="cancel" size={30} color="red" />
-            </Pressable>
-          </Modal>
-        )}
       </View>
-    </>
+
+      <DefaultInput
+        customInput={!isCoinValid ? "border-red-500" : ""}
+        label="Coin"
+        placeholder="1"
+        customCss="w-[95%] mx-auto"
+        value={coin}
+        onChangeText={handleCoinChange}
+      />
+
+      <View
+        style={tailwind`w-full flex flex-row justify-center items-center mt-5`}
+      >
+        <FontAwesome6 name="arrow-down" size={24} color="white" />
+        <FontAwesome6 name="arrow-up" size={24} color="white" />
+      </View>
+
+      <DefaultInput
+        customInput={!isAmountValid ? "border-red-500" : ""}
+        label="Amount (Naira)"
+        placeholder="25"
+        customCss="w-[95%] mx-auto mt-5"
+        value={amount}
+        onChangeText={handleAmountChange}
+      />
+
+      <View style={tailwind`mt-10 w-[95%] mx-auto`}>
+        {!(isAmountValid && isCoinValid) ? (
+          <DisabledButton text="Continue" />
+        ) : (
+          <DefaultButton onPress={handlePaystackPayment} text="Continue" />
+        )}
+
+        <Pressable onPress={() => router.push("/Pages/")}>
+          <Text
+            style={tailwind`text-[#00A859] mx-auto mt-3 text-[18px] underline`}
+          >
+            Cancel
+          </Text>
+        </Pressable>
+      </View>
+
+      {loading && (
+        <ActivityIndicator size="large" color="green" style={tailwind`mt-5`} />
+      )}
+
+      {showPaystack && paymentUrl && (
+        <Modal visible={showPaystack} animationType="slide">
+          <WebView
+            style={tailwind`mt-[18%]`}
+            source={{ uri: paymentUrl }}
+            onNavigationStateChange={(navState) => {
+              if (navState.url.includes("payment successful")) {
+                handleWebViewClose();
+                Alert.alert(
+                  "Payment Verified",
+                  "Your payment has been successfully verified!"
+                );
+              }
+            }}
+          />
+          <Pressable
+            style={tailwind`absolute top-10 right-10`}
+            onPress={handleWebViewClose}
+          >
+            <MaterialIcons name="cancel" size={30} color="red" />
+          </Pressable>
+        </Modal>
+      )}
+    </View>
   );
 };
 
